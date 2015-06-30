fb3b789d01344476a79454aae771c35ac768be2f
Sam Buck <swb5075@uncw.edu>
2015-06-30 12:58:31 -0400

    Added documentation and added line breaks to conform with Pythons recommended line length

--- Developing Release/Winnow-Devel/gwas.py
+++ Developing Release/Winnow-Devel/gwas.py
@@ -2,12 +2,54 @@
 Performs functions necessary for GWAS analysis 
 """
 
-
 from performetrics import *
 
 
 def gwasWithBeta(betaColumn, betaTrueFalse, snpTrueFalse, scoreColumn, threshold):
-	return ["rmse", "mae", "r", "r2", "auc", "TruePositives", "FalsePositives", "TrueNegatives", "FalseNegatives", "TruePosRate", "FalsePosRate", "error", "sens", "spec", "precision", "youden"], [rmse(betaColumn, betaTrueFalse), mae(betaColumn, betaTrueFalse), r(betaColumn, betaTrueFalse), r2(betaColumn, betaTrueFalse),auc(snpTrueFalse, scoreColumn), tp(snpTrueFalse, threshold, scoreColumn), fp(snpTrueFalse, threshold, scoreColumn), tn(snpTrueFalse, threshold, scoreColumn), fn(snpTrueFalse, threshold, scoreColumn), tpr(snpTrueFalse, threshold, scoreColumn), fpr(snpTrueFalse, threshold, scoreColumn), error(snpTrueFalse, threshold, scoreColumn), sens(snpTrueFalse, threshold, scoreColumn), spec(snpTrueFalse, threshold, scoreColumn), precision(snpTrueFalse, threshold, scoreColumn), youden(snpTrueFalse, threshold, scoreColumn)]
+    """
+    Performs GWAS analysis with Beta
+
+    :param betaColumn: collected positions
+    :param betaTrueFalse: known of the collected positions
+    :param snpTrueFalse: true/false data set
+    :param scoreColumn: score data set
+    :param threshold: significant threshold
+    :return: an array of functions and an array of those functions' results
+    """
+    return ["rmse", "mae", "r", "r2", "auc", "TruePositives", "FalsePositives", "TrueNegatives", "FalseNegatives",
+            "TruePosRate", "FalsePosRate", "error", "sens", "spec", "precision", "youden"], \
+           [rmse(betaColumn, betaTrueFalse), mae(betaColumn, betaTrueFalse), r(betaColumn, betaTrueFalse),
+            r2(betaColumn, betaTrueFalse), auc(snpTrueFalse, scoreColumn), tp(snpTrueFalse, threshold, scoreColumn),
+            fp(snpTrueFalse, threshold, scoreColumn), tn(snpTrueFalse, threshold, scoreColumn), fn(snpTrueFalse,
+                                                                                                   threshold,
+                                                                                                   scoreColumn),
+            tpr(snpTrueFalse, threshold, scoreColumn), fpr(snpTrueFalse, threshold,
+                                                           scoreColumn), error(snpTrueFalse, threshold, scoreColumn),
+            sens(snpTrueFalse, threshold, scoreColumn),
+            spec(snpTrueFalse, threshold, scoreColumn), precision(snpTrueFalse, threshold, scoreColumn),
+            youden(snpTrueFalse, threshold, scoreColumn)]
+
 
 def gwasWithoutBeta(snpTrueFalse, scoreColumn, threshold):
-	return ["auc", "TruePositives", "FalsePositives", "TrueNegatives", "FalseNegatives", "TruePosRate", "FalsePosRate", "error", "sens", "spec", "precision", "youden"], [auc(snpTrueFalse, scoreColumn), tp(snpTrueFalse, threshold, scoreColumn), fp(snpTrueFalse, threshold, scoreColumn), tn(snpTrueFalse, threshold, scoreColumn), fn(snpTrueFalse, threshold, scoreColumn),tpr(snpTrueFalse, threshold, scoreColumn), fpr(snpTrueFalse, threshold, scoreColumn), error(snpTrueFalse, threshold, scoreColumn), sens(snpTrueFalse, threshold, scoreColumn), spec(snpTrueFalse, threshold, scoreColumn), precision(snpTrueFalse, threshold, scoreColumn), youden(snpTrueFalse, threshold, scoreColumn)]
+    """
+    Performs GWAS analysis without Beta
+
+    :param snpTrueFalse: true/false data set
+    :param scoreColumn: score data set
+    :param threshold: significant threshold
+    :return: an array of functions and an array of those functions' results
+    """
+    return ["auc", "TruePositives", "FalsePositives", "TrueNegatives", "FalseNegatives", "TruePosRate", "FalsePosRate",
+            "error", "sens", "spec", "precision", "youden"], [auc(snpTrueFalse, scoreColumn),
+                                                              tp(snpTrueFalse, threshold, scoreColumn),
+                                                              fp(snpTrueFalse, threshold, scoreColumn),
+                                                              tn(snpTrueFalse, threshold, scoreColumn),
+                                                              fn(snpTrueFalse, threshold, scoreColumn),
+                                                              tpr(snpTrueFalse, threshold, scoreColumn),
+                                                              fpr(snpTrueFalse, threshold, scoreColumn),
+                                                              error(snpTrueFalse, threshold, scoreColumn),
+                                                              sens(snpTrueFalse, threshold, scoreColumn),
+                                                              spec(snpTrueFalse, threshold, scoreColumn),
+                                                              precision(snpTrueFalse, threshold, scoreColumn),
+                                                              youden(snpTrueFalse, threshold, scoreColumn)]
+
