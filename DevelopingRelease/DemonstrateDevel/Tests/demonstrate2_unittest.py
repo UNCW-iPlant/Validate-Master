9fc24271fc18531fbdfa571648ab570600dd8374
Talleyman <stephen522@earthlink.net>
2015-08-10 13:00:29 -0400

    Dem2 unit test

--- DevelopingRelease/DemonstrateDevel/Tests/demonstrate2_unittest.py
+++ DevelopingRelease/DemonstrateDevel/Tests/demonstrate2_unittest.py
@@ -18,14 +18,14 @@ class Dem2Test(unittest.TestCase):
     def test_outputs(self):
         self.load_r()
         r_dem2 = robjects.globalenv['Demonstrate2']
-        r_dem2(self.testdir)
         outputnames = ['Comparison Table.csv', 'TP Histograms.pdf', 'FP Histograms.pdf', 'True Positives vs. False Positives.pdf', 'Plot of AUC by MAE.pdf']
         for name in outputnames:
             if os.path.isfile(self.testdir + '/' + name):
                 os.remove(self.testdir + '/' + name)
+        r_dem2(self.testdir)
+        for name in outputnames:
             self.assertTrue(os.path.isfile(self.testdir + '/' + name))
             self.assertTrue(os.path.getsize(self.testdir + '/'+ name) > 0)
-        
     def load_r(self):
         with open(os.getcwd()[:os.getcwd().index('Tests')]+'DemoMPlot/R/Demonstrate2.R') as f:
             dem2 = f.read()
