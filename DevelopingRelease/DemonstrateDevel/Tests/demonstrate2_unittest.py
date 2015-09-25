08ee5ac4306a79eb28cd708ad0a79f0c27a3ae3e
Sam Buck <swb5075@uncw.edu>
2015-09-25 16:25:31 -0400

    Demo/Demo2 Tests Update - This got reverted.

--- DevelopingRelease/DemonstrateDevel/Tests/demonstrate2_unittest.py
+++ DevelopingRelease/DemonstrateDevel/Tests/demonstrate2_unittest.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import rpy2.robjects as robjects
 
+
 class Dem2Test(unittest.TestCase):
     testdir = os.getcwd()+"/Dem2TestFiles"
 
@@ -11,27 +12,29 @@ class Dem2Test(unittest.TestCase):
     def test_function(self):
         self.load_r()
         r_dem2 = robjects.globalenv['Demonstrate2']
-        r_dem2(self.testdir)
         return_value = str(r_dem2(self.testdir))
-        self.assertEquals(return_value, 'Done!')
+        self.assertEquals(return_value, '[1] "Done!"\n')
         
     def test_outputs(self):
         self.load_r()
         r_dem2 = robjects.globalenv['Demonstrate2']
-        outputnames = ['Comparison Table.csv', 'TP Histograms.pdf', 'FP Histograms.pdf', 'True Positives vs. False Positives.pdf', 'Plot of AUC by MAE.pdf']
+        outputnames = ['ComparisonTable.csv', 'TP Histograms.pdf', 'FP Histograms.pdf',
+                       'True Positives vs. False Positives.pdf', 'Plot of AUC by MAE.pdf']
         for name in outputnames:
             if os.path.isfile(self.testdir + '/' + name):
                 os.remove(self.testdir + '/' + name)
         r_dem2(self.testdir)
         for name in outputnames:
             self.assertTrue(os.path.isfile(self.testdir + '/' + name))
-            self.assertTrue(os.path.getsize(self.testdir + '/'+ name) > 0)
+            self.assertTrue(os.path.getsize(self.testdir + '/' + name) > 0)
+            os.remove(self.testdir + '/' + name)
         
     def load_r(self):
         with open(os.getcwd()[:os.getcwd().index('Tests')]+'DemoMPlot/R/Demonstrate2.R') as f:
             dem2 = f.read()
         robjects.r(dem2)
-    
+
+
 def get_test_suite():
     return unittest.TestLoader().loadTestsFromTestCase(Dem2Test)
 
--- DevelopingRelease/DemonstrateDevel/Tests/demonstratedevel_test_suite.py
+++ DevelopingRelease/DemonstrateDevel/Tests/demonstratedevel_test_suite.py
@@ -1,5 +1,7 @@
 import ListRanker_unittest as lr
 import MPlot_unittest as mplot
+import demonstrate_unittest as dem
+import demonstrate2_unittest as dem2
 import unittest
 
 
@@ -8,6 +10,8 @@ def main():
 
     test_suite.addTests(lr.get_test_suite())
     test_suite.addTests(mplot.get_test_suite())
+    test_suite.addTests(dem.get_test_suite())
+    test_suite.addTests((dem2.get_test_suite()))
 
     unittest.TextTestRunner(verbosity=2).run(test_suite)
 
